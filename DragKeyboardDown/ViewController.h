f461dfcf011cde2f05001fdabb200b961443d33f
Tu Yimin <Yimin@Tu-YiminmatoMacBook-Pro.local>
2012-03-21 14:04:01 +0800

    Initial Commit

---
+++ DragKeyboardDown/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  DragKeyboardDown
+//
+//  Created by Yimin Tu on 12-3-21.
+//  Copyright (c) 2012年 dianping.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ DragKeyboardDown/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DragKeyboardDown
+//
+//  Created by Yimin Tu on 12-3-21.
+//  Copyright (c) 2012年 dianping.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
